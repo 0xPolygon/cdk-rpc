8dbb68fe396afca6bcdc28f7d549ff02c55753b9
Victor Castell <0x@vcastellm.xyz>
2024-03-05 18:19:01 +0100

    typo

--- rpc/options.go
+++ rpc/options.go
@@ -2,7 +2,7 @@ package rpc
 
 import "net/http"
 
-func WithHealthcheckHandler(h http.Handler) Option {
+func WithHealthHandler(h http.Handler) Option {
 	return func(s *Server) {
 		s.healthHandler = h
 	}
